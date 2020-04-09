bb1a6d087e4f36afaec2ec1168cf4943cf5458b1
sherif <sherifyounuse@gmail.com>
2020-04-09 13:57:37 +0530

    shop details

--- index.js
+++ index.js
@@ -68,4 +68,4 @@ client.close();
 
 
 
-app.listen(3000, () => console.log(`server listening on port 3000!`))
+app.listen(process.env.PORT||3000, () => console.log(`server listening on port 3000!`))
